d569b22620ea52dd65211784296d90e57d17b6a2
Ariunmunkh.e <is11d011@gmail.com>
2017-11-26 16:16:51 +0800

    Menu tree list

--- DXWebMRCS/Controllers/HomeController.cs
+++ DXWebMRCS/Controllers/HomeController.cs
@@ -30,6 +30,70 @@ namespace DXWebMRCS.Controllers
             var menuList = db.Menus.ToList();
             return View(menuList);
         }
-    
+
+        #region Menu code
+        public ActionResult TreeListPartial()
+        {
+            return PartialView(NorthwindDataProvider.GetMenus());
+        }
+
+        public ActionResult TreeListAddNewPartial(Menu Menu)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    NorthwindDataProvider.InsertMenu(Menu);
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+
+            return PartialView("TreeListPartial", NorthwindDataProvider.GetMenus());
+        }
+
+        public ActionResult TreeListUpdatePartial(Menu Menu)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    NorthwindDataProvider.UpdateMenu(Menu);
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+
+            return PartialView("TreeListPartial", NorthwindDataProvider.GetMenus());
+        }
+
+        public ActionResult TreeListMovePartial(int ID, int? PARENTID)
+        {
+            NorthwindDataProvider.MoveMenu(ID, Convert.ToInt32(PARENTID));
+            return PartialView("TreeListPartial", NorthwindDataProvider.GetMenus());
+        }
+
+        public ActionResult TreeListDeletePartial(int ID)
+        {
+            try
+            {
+                NorthwindDataProvider.DeleteMenu(ID);
+            }
+            catch (Exception e)
+            {
+                ViewData["EditError"] = e.Message;
+            }
+
+            return PartialView("TreeListPartial", NorthwindDataProvider.GetMenus());
+        }
+        #endregion
     }
 }
---
+++ DXWebMRCS/Models/NorthwindDataProvider.cs
@@ -0,0 +1,103 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Web.Configuration;
+using DXWebMRCS.Models;
+
+public class NorthwindDataProvider
+{
+    public static IEnumerable GetMenus()
+    {
+        List<Menu> Menu = new List<Menu>();
+
+        using (OleDbConnection connection = new OleDbConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+        {
+            OleDbCommand selectCommand = new OleDbCommand("SELECT * FROM Menu", connection);
+
+            connection.Open();
+
+            OleDbDataReader reader = selectCommand.ExecuteReader(CommandBehavior.CloseConnection);
+
+            while (reader.Read())
+            {
+                Menu.Add(new Menu()
+                {
+                    MenuID = (int)reader["MenuID"],
+                    NameMon = (string)reader["NameMon"],
+                    NameEng = (string)reader["NameEng"],
+                    NavigateUrl = (string)reader["NavigateUrl"],
+                    ParentId = (reader["ParentId"] == DBNull.Value ? null : (int?)reader["ParentId"])
+                });
+            }
+
+            reader.Close();
+        }
+
+        return Menu;
+    }
+
+    public static void InsertMenu(Menu Menu)
+    {
+        using (OleDbConnection connection = new OleDbConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+        {
+            OleDbCommand insertCommand = new OleDbCommand("INSERT INTO [Menu] ([NameMon], [NameEng], [NavigateUrl], [ParentId]) VALUES (?, ?, ?, ?)", connection);
+
+            insertCommand.Parameters.AddWithValue("NameMon", Menu.NameMon);
+            insertCommand.Parameters.AddWithValue("NameEng", Menu.NameEng);
+            insertCommand.Parameters.AddWithValue("NavigateUrl", Menu.NavigateUrl);
+
+            if (Menu.ParentId.HasValue)
+                insertCommand.Parameters.AddWithValue("ParentId", Menu.ParentId);
+            else
+                insertCommand.Parameters.AddWithValue("ParentId", DBNull.Value);
+
+            connection.Open();
+            insertCommand.ExecuteNonQuery();
+        }
+    }
+
+    public static void UpdateMenu(Menu Menu)
+    {
+        using (OleDbConnection connection = new OleDbConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+        {
+            OleDbCommand updateCommand = new OleDbCommand("UPDATE [Menu] SET [NameMon] = ?, [NameEng] = ?, [NavigateUrl] = ?, [ParentId] = ? WHERE [MenuID] = ?", connection);
+
+            updateCommand.Parameters.AddWithValue("NameMon", Menu.NameMon);
+            updateCommand.Parameters.AddWithValue("NameEng", Menu.NameEng);
+            updateCommand.Parameters.AddWithValue("NavigateUrl", Menu.NavigateUrl);
+            updateCommand.Parameters.AddWithValue("ParentId", Menu.ParentId);
+            updateCommand.Parameters.AddWithValue("MenuID", Menu.MenuID);
+
+            connection.Open();
+            updateCommand.ExecuteNonQuery();
+        }
+    }
+
+    public static void MoveMenu(int MenuID, int ParentId)
+    {
+        using (OleDbConnection connection = new OleDbConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+        {
+            OleDbCommand updateCommand = new OleDbCommand("UPDATE [Menu] SET [ParentId] = ? WHERE [MenuID] = ?", connection);
+
+            updateCommand.Parameters.AddWithValue("ParentId", ParentId);
+            updateCommand.Parameters.AddWithValue("MenuID", MenuID);
+
+            connection.Open();
+            updateCommand.ExecuteNonQuery();
+        }
+    }
+
+    public static void DeleteMenu(int MenuID)
+    {
+        using (OleDbConnection connection = new OleDbConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+        {
+            OleDbCommand deleteCommand = new OleDbCommand("DELETE FROM Menu WHERE MenuID = " + MenuID.ToString(), connection);
+
+            connection.Open();
+            deleteCommand.ExecuteNonQuery();
+        }
+    }
+}
