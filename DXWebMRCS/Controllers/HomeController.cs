d703e8666594023ef0fc1c211479a1e0a82f9c92
Ariunmunkh.e <is11d011@gmail.com>
2017-11-26 15:33:11 +0800

    menu

--- DXWebMRCS/Controllers/HomeController.cs
+++ DXWebMRCS/Controllers/HomeController.cs
@@ -9,17 +9,26 @@ namespace DXWebMRCS.Controllers
 {
     public class HomeController : Controller
     {
+        private UsersContext db = new UsersContext();
         public ActionResult Index()
         {
             // DXCOMMENT: Pass a data model for GridView
-            
-            return View();    
+            var menuList = db.Menus.ToList();
+            return View(menuList);    
         }
         
         public ActionResult GridViewPartialView() 
         {
             // DXCOMMENT: Pass a data model for GridView in the PartialView method's second parameter
             return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult _HeaderPartial()
+        {
+            // DXCOMMENT: Pass a data model for GridView in the PartialView method's second parameter
+            var menuList = db.Menus.ToList();
+            return View(menuList);
         }
     
     }
