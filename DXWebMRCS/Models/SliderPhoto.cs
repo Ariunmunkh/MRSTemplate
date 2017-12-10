b3e0c1ca6ede789f4e4f7d2bf1625a91bc573968
Ariunmunkh <is11d011@gmail.com>
2017-12-10 16:18:34 +0800

    Big Muugii

---
+++ DXWebMRCS/Models/SliderPhoto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DXWebMRCS.Models
+{
+    public class SliderPhoto
+    {
+        public int Id { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
