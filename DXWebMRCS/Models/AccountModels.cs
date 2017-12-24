680683b02d5183d78eabf54c4ffc79a12e16794f
Ariunmunkh.e <is11d011@gmail.com>
2017-12-24 13:20:39 +0800

    Anka

--- DXWebMRCS/Models/AccountModels.cs
+++ DXWebMRCS/Models/AccountModels.cs
@@ -24,6 +24,7 @@ namespace DXWebMRCS.Models {
         public DbSet<SliderPhoto> SliderPhotos { get; set; }
         public DbSet<Branch> Branchs { get; set; }
         public DbSet<Training> Trainings { get; set; }
+        public DbSet<TrainingRequest> TrainingRequests { get; set; }
     }
     [Table("UserProfile")]
     public class UserProfile {
---
+++ DXWebMRCS/Models/TrainingRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DXWebMRCS.Models
+{
+    public class TrainingRequest
+    {
+        public int ID { get; set; }
+
+        public int TrainingID { get; set; }
+      
+        public int UserID { get; set; }
+        /*
+         * 0-Хүсэлт илгээсэн
+         * 1-Хүлээн авсан
+         * 9-Цуцалсан
+        */
+        public int Status { get; set; }
+    }
+}
